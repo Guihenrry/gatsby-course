838acbe52b2396bd4c3ea63d495d6c2ecc80127a
Gui <guilhermehenry60@gmail.com>
2020-01-18 16:33:36 -0300

    Add blog post style

---
+++ src/components/Pagination/styled.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const PaginationWrapper = styled.section`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.5rem 3rem;
+  color: #8899ae;
+
+  a {
+    color: #8899ae;
+    text-decoration: none;
+    transition: color 0.5s;
+
+    &:hover {
+      color: #1fa1f2;
+    }
+  }
+`
---
+++ src/templates/blog-list.js
@@ -0,0 +1,80 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+import PostItem from "../components/PostItem"
+import Pagination from "../components/Pagination"
+
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
+
+  const { currentPage, numPages } = props.pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage === 2 ? `/` : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
+
+  return (
+    <Layout>
+      <SEO title="HOME" />
+
+      {postList.map(
+        ({
+          node: {
+            fields: { slug },
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+          },
+        }) => (
+          <PostItem
+            key={slug}
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            description={description}
+            title={title}
+          />
+        )
+      )}
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+        }
+      }
+    }
+  }
+`
+
+export default BlogList
